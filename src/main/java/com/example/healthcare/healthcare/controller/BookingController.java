4ec0bf13fcb69ab824e22bff2009bef47cc06216
Sami-115667 <shamsurrahman07052001@gmail.com>
2025-01-24 16:32:45 +0600

    added functionality

--- src/main/java/com/example/healthcare/healthcare/controller/BookingController.java
+++ src/main/java/com/example/healthcare/healthcare/controller/BookingController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
+@CrossOrigin(origins= "http://localhost:3000")
 @RestController
 @RequestMapping(value = "/api/booking")
 public class BookingController {
